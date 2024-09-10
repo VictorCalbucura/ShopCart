8a677b565f287899b472ddf1dd743e901638ba75
VictorCalbucura <111377379+VictorCalbucura@users.noreply.github.com>
2024-09-10 17:31:03 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
 import './App.css'
-import Header from './components/header'
-import FilterPrice from './components/filter'
+import { useState } from 'react'
+import { products as initialProducts } from './mocks/products.json'
+import Products from './components/products'
+import Filters from './components/filter'
+import Cart from './components/cart'
+import { CartProvider } from './contexts/CartContext'
 
 function App() {
-  return (
-    <>
-      <Header />
+  const [products] = useState(initialProducts)
+  const [filters, setFilters] = useState({
+    category: 'all',
+    price: 0
+  })
 
-      <FilterPrice />
-    </>
+  const filterProducts = (products) => {
+    return products.filter((product) => {
+      return (
+        (filters.category === 'all' || product.category === filters.category) &&
+        (filters.price === 0 || product.price >= filters.price)
+      )
+    })
+  }
+
+  return (
+    <CartProvider>
+      <h1>Shoping Cart</h1>
+      <Cart />
+      <Filters filters={filters} changeFilters={setFilters} />
+      <Products products={filterProducts(products)} />
+    </CartProvider>
   )
 }
 
